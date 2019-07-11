ee7dd0f959287f5d2748cdcf1f8ca247509b3def
Rob Wells <rob@robjwells.com>
2019-07-11 01:06:47 +0100

    Ex 1.1

---
+++ exercises/Ex1_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Ex1_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(Integer.toBinaryString(n));
+        System.out.printf("%o\n", n);
+        System.out.printf("%x\n", n);
+        if (n != 0) {
+            System.out.printf("1/n = %a\n", 1.0/n);
+        }
+    }
+}
