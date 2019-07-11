33c296b917a7aae55ae35d5fb32fd7cfcb203a66
Rob Wells <rob@robjwells.com>
2019-07-11 23:05:07 +0100

    Ex 1.4: Tidy up

--- exercises/Ex1_4.java
+++ exercises/Ex1_4.java
@@ -4,9 +4,8 @@ class Ex1_4 {
         System.out.println(Double.MIN_VALUE);
         System.out.println(Double.MAX_VALUE);
 
-        System.out.println("Using Double.nextUp():");
+        System.out.println("Using Double.nextUp() and Double.nextDown():");
         System.out.println(Math.nextUp(0.0));
         System.out.println(Math.nextDown(Double.POSITIVE_INFINITY));
-       
     }
 }
