12d9f3e2233437970307925fed6e0bf997412756
Rob Wells <rob@robjwells.com>
2019-07-11 23:24:03 +0100

    Ex 1.6

---
+++ exercises/Ex1_6.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Ex1_6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        BigInteger factorial = BigInteger.valueOf(n);
+        for (
+            BigInteger current = factorial.subtract(BigInteger.ONE);
+            current.compareTo(BigInteger.ZERO) > 0;
+            current = current.subtract(BigInteger.ONE)
+        ) {
+            factorial = factorial.multiply(current);
+        }
+        System.out.printf("Factorial of %s = %s\n", n, factorial);
+}
+}
