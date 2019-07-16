6d83d621a006753f17cd93af4f93eceda9c716f1
Rob Wells <rob@robjwells.com>
2019-07-16 09:22:14 +0100

    Ex 1.11

---
+++ exercises/Ex1_11.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+class Ex1_11 {
+    public static void main(String[] args) {
+        String input = args[0];
+        IntStream.range(0, input.length())
+            .filter(index -> input.codePointAt(index) > 127)
+            .mapToObj(index -> input.substring(index, index + 1))
+            .map(c -> String.format("%s\t%s", c, c.codePointAt(0)))
+            .forEach(System.out::println);
+    }
+}
