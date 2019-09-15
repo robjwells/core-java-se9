8d66b46039b6728ae883c18c4b5b136fe2faa2a7
Rob Wells <rob@robjwells.com>
2019-09-15 13:56:03 +0100

    Ex 2.8: Explore code-generation in IntelliJ

---
+++ exercises-project/src/ch02/ex08/Main.java
@@ -0,0 +1,45 @@
+package ch02.ex08;
+
+import java.util.Objects;
+
+public class Main {
+    /**
+     * The methods in the <code>Point</code> class below were entirely
+     * automatically generated after I wrote in the two private field
+     * declarations for <code>x</code> and <code>y</code>.
+     */
+    public static class Point {
+        private double x;
+        private double y;
+
+        public Point() {
+        }
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return Double.compare(point.x, x) == 0 &&
+                    Double.compare(point.y, y) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+}
