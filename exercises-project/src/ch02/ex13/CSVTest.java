efaa78cecbe9e377b3364c7c6bbd8d6b74265511
Rob Wells <rob@robjwells.com>
2019-09-20 07:24:48 +0100

    Ex 2.13: Try (and fail) to get OpenCSV working

---
+++ exercises-project/src/ch02/ex13/CSVTest.java
@@ -0,0 +1,30 @@
+package ch02.ex13;
+
+// import com.opencsv.CSVReader;
+
+import java.io.StringReader;
+import java.util.Arrays;
+
+/**
+ * Ex 2.13.
+ *
+ * I tried adding the OpenCSV jar alone and got an error.
+ *
+ * I added the library referenced in the error (org.apache.commons.lang3) and
+ * got the same error.
+ *
+ * I added all of OpenCSV's dependencies and still got an error.
+ *
+ * I give up.
+ *
+ * Commented out below because I removed the OpenCSV jar.
+ */
+public class CSVTest {
+//    public static void main(String[] args) throws Exception {
+//        CSVReader reader = new CSVReader(new StringReader("a,b,c\nd,e,f\ng,h,i"));
+//        String[] nextLine;
+//        while ((nextLine = reader.readNext()) != null) {
+//            System.out.println(Arrays.toString(nextLine));
+//        }
+//    }
+}
