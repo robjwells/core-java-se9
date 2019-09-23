55c994c4a958a54a9444fdca74a13001d59069be
Rob Wells <rob@robjwells.com>
2019-09-23 21:54:21 +0100

    Ex 3.2

---
+++ exercises-project/src/ch03/ex02/ex_3_02.java
@@ -0,0 +1,32 @@
+package ch03.ex02;
+
+/*
+ * What are the supertypes of String?
+ *
+ * String (itself)
+ * Object (superclass)
+ * Serializable (interface)
+ * Comparable<String> (interface)
+ * CharSequence (interface)
+ * Constable (interface)
+ * ConstantDesc (interface)
+ *
+ * What are the supertypes of Scanner?
+ *
+ * Scanner (itself)
+ * Object (superclass)
+ * Closeable (interface)
+ * AutoCloseable (interface)
+ * Iterator<String> (interface)
+ *
+ * What are the supertypes of ImageOutputStream?
+ *
+ * ImageOutputStream (itself)
+ * AutoCloseable (interface)
+ * Closeable (interface)
+ * DataInput (interface)
+ * DataOutput (interface)
+ * ImageInputStream (interface)
+ */
+public class ex_3_02 {
+}
