fc8ecebc4fed2f6f0fc2126f61f1bec67b8e861f
Rob Wells <rob@robjwells.com>
2019-11-02 11:34:25 +0000

    Ex 5.3

--- exercises-project/src/ch05/ex01/ReadValues.java
+++ exercises-project/src/ch05/ex01/ReadValues.java
@@ -33,4 +33,20 @@ public class ReadValues {
     public static double sumOfValues(String filename) throws FileNotFoundException {
         return readValues(filename).stream().mapToDouble(d -> d).sum();
     }
+
+    /**
+     * Exercise 5.3: Call the <tt>sumOfValues</tt> method and provide feedback to the user.
+     * @param args The first command-line argument should be the filename to read.
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide the filename as the first and only argument.");
+            return;
+        }
+        try {
+            System.out.println(sumOfValues(args[0]));
+        } catch (FileNotFoundException exc) {
+            System.err.printf("Could not find file %s.\n", args[0]);
+        }
+    }
 }
